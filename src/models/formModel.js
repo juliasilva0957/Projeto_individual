8c0cc9af55449d83a5066d12a8ba6abbac2f4bf8
juliasilva0957 <julia.silva@sptech.school>
2026-04-26 12:11:56 -0300

    Tela sobre e cadastro prontas + validações de senha

--- src/models/formModel.js
+++ src/models/formModel.js
@@ -2,7 +2,7 @@ var database = require("../database/config");
 
 function cadastrarInformacoes(tipo, raca, senteAjudou , tipoTranstorno , teve , descricao, fkUsuario) {
 
-    var instrucaoSql = `INSERT INTO formCadastro(transtornoMental, momentosDificei, comoAjudou, especie, raça, fkUsuario) VALUES(
+    var instrucaoSql = `INSERT INTO formCadastro(transtornoMental, momentosDificeis, comoAjudou, especie, raça, fkUsuario) VALUES(
         '${tipoTranstorno}', 
         ${senteAjudou},
         '${descricao}',
@@ -18,14 +18,3 @@ function cadastrarInformacoes(tipo, raca, senteAjudou , tipoTranstorno , teve , 
 module.exports = {
     cadastrarInformacoes
 }
-// CREATE TABLE formCadastro (
-// idForm INT PRIMARY KEY AUTO_INCREMENT ,
-// transtornoMental BOOLEAN,
-// momentosDificeis BOOLEAN,
-// comoAjudou VARCHAR(255),
-// especie BOOLEAN,
-// raça VARCHAR(50),
-// fkUsuario INT,
-// CONSTRAINT chFkUsuario
-// FOREIGN KEY (fkUsuario) REFERENCES usuario(idUsuario)
-// );
